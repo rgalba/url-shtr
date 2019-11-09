8670ed04510f92619426b2da8c82782ae525f222
Rodrigo Galba Miranda <rodrigogalba@gmail.com>
2019-11-09 12:29:03 -0300

    add reverse function configuration

--- src/main/java/me/rgalba/URL/Shortener/UrlShrtApplication.java
+++ src/main/java/me/rgalba/URL/Shortener/UrlShrtApplication.java
@@ -1,26 +1,21 @@
 package me.rgalba.URL.Shortener;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-import java.net.URL;
-import java.util.Scanner;
+import java.util.function.Function;
 
 @SpringBootApplication
-public class UrlShrtApplication implements CommandLineRunner {
+public class UrlShrtApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UrlShrtApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("Inform the URL:");
-		System.out.println("ex: https://mysite.com");
-		Scanner scanner = new Scanner(System.in);
-		String urlInput = scanner.nextLine();
-		URL url = new URL(urlInput);
-		System.out.println("URL received: " + url);
+	@Bean
+	public Function<String, String> reverse() {
+		return value -> new StringBuilder(value).reverse().toString();
 	}
+
 }
